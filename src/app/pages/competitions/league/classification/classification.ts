084eff9948f0299819e9e19937aeaf984b6387ec
Sergi <nogues.s.6@gmail.com>
2018-11-24 23:14:39 +0100

    Premios (puntos) para liga individual

--- src/app/pages/competitions/league/classification/classification.ts
+++ src/app/pages/competitions/league/classification/classification.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { AppConfig } from '../../../../app.config';
-import { Login, Student, Role, Competition, Journey, Match, Team } from '../../../../shared/models/index';
-import { LoadingService, UtilsService, AlertService, TeamService,
-  CompetitionService, JourneyService } from '../../../../shared/services/index';
+import { Login, Student, Role, Competition, Journey, Match, Team, Point, PointRelation, School } from '../../../../shared/models/index';
+import { TranslateService } from 'ng2-translate/ng2-translate';
+import { LoadingService, UtilsService, AlertService, TeamService, CompetitionService,
+   JourneyService, PointRelationService, PointService, SchoolService, GroupService } from '../../../../shared/services/index';
 
 @Component({
   selector: 'app-classification',
@@ -22,6 +23,24 @@ export class ClassificationComponent implements OnInit {
   public odd: boolean;
   public matchesJourneys: Array<Array<Match>>;
 
+  // PREMIOS
+    // BestParticipants
+    public GroupIdAwards: string;
+    public SchoolIdAwards: string;
+      public Primer_name: string;
+      public Primer_Id: string;
+      public Segon_name: string;
+      public Segon_Id: string;
+
+    // Point
+    public Primer_p_value: number; // Valor del punto que se le enviará al 1er clasificado
+    public Primer_point: Point;
+    public Primer_PointRelation: PointRelation;
+    public Segon_p_value: number; // Valor del punto que se le enviará al 2do clasificado
+    public Segon_point: Point;
+    public Segon_PointRelation: PointRelation;
+
+
   public scores = new Array<Score>();
   public score: Score;
 
@@ -29,6 +48,11 @@ export class ClassificationComponent implements OnInit {
     public loadingService: LoadingService,
     public alertService: AlertService,
     public journeyService: JourneyService,
+    public schoolService: SchoolService,
+    public pointService: PointService,
+    public pointRelationService: PointRelationService,
+    public groupService: GroupService,
+    public translateService: TranslateService,
     private route: ActivatedRoute,
     private teamService: TeamService,
     private competitionService: CompetitionService) {
@@ -166,8 +190,93 @@ export class ClassificationComponent implements OnInit {
       for (let _s = 0; _s < this.scores.length; _s++) {
         this.scores[_s].position = _s + 1;
        }
-       this.loadingService.hide();
+       this.SetBestParticipants();
   }
+
+  SetBestParticipants(): void {
+    for (let _m = 0; _m < this.participants.length; _m++) {
+      if (this.scores[0].name === this.participants[_m].name) {
+        this.Primer_Id = this.participants[_m].id.toString();
+      }
+      if (this.scores[1].name === this.participants[_m].name) {
+        this.Segon_Id = this.participants[_m].id.toString();
+      }
+    }
+    this.Primer_name = this.competition.name.toString() + '_1er Clasificado_' + this.scores[0].name.toString();
+    this.Segon_name = this.competition.name.toString() + '_2do Clasificado_' + this.scores[1].name.toString();
+    this.GroupIdAwards = this.competition.groupId.toString();
+    this.groupService.getMyGroupStudents(this.GroupIdAwards).subscribe(
+      ( (students: Array<Student>) => {
+        for (let _n = 0; _n < students.length; _n++) {
+          if (this.Primer_Id === students[_n].id ) {
+            this.SchoolIdAwards = students[_n].schoolId.toString();
+          }
+        }
+        this.loadingService.hide();
+      }),
+      ((error: Response) => {
+        this.loadingService.hide();
+        this.alertService.show(error.toString());
+      }));
+
+  }
+
+  SendPoint1(): void {
+    if (this.scores[0].points !== 0) {
+      this.pointService.savePoint(this.Primer_name, this.Primer_p_value, 'url_image').subscribe(
+        ((newPoint: Point) => {
+          this.Primer_point = newPoint;
+          this.SetPoint1();
+        }),
+        ((error: Response) => {
+          this.loadingService.hide();
+          this.alertService.show(error.toString());
+        }));
+    } else { this.alertService.show(this.translateService.instant('CLASSIFICATION.NOAWARDS')); }
+  }
+
+  SendPoint2(): void {
+    if (this.scores[0].points !== 0) {
+      this.pointService.savePoint(this.Segon_name, this.Segon_p_value, 'url_image').subscribe(
+        ((newPoint: Point) => {
+          this.Segon_point = newPoint;
+          this.SetPoint2();
+        }),
+        ((error: Response) => {
+          this.loadingService.hide();
+          this.alertService.show(error.toString());
+        }));
+    } else { this.alertService.show(this.translateService.instant('CLASSIFICATION.NOAWARDS')); }
+  }
+
+  SetPoint1(): void {
+    this.pointRelationService.postPointRelation(this.Primer_point.id, this.Primer_Id, this.SchoolIdAwards,
+    this.GroupIdAwards, 1).subscribe(
+    ((responsePointRelation: PointRelation) => {
+      this.Primer_PointRelation = responsePointRelation;
+      this.loadingService.hide();
+      this.alertService.show(this.translateService.instant('POINTS.CORASSIGN'));
+    }),
+    ((error: Response) => {
+      this.loadingService.hide();
+      this.alertService.show(error.toString());
+    }));
+  }
+
+  SetPoint2(): void {
+    this.pointRelationService.postPointRelation(this.Segon_point.id, this.Segon_Id, this.SchoolIdAwards,
+      this.GroupIdAwards, 1).subscribe(
+      ((responsePointRelation: PointRelation) => {
+        this.Primer_PointRelation = responsePointRelation;
+        this.loadingService.hide();
+        this.alertService.show(this.translateService.instant('POINTS.CORASSIGN'));
+      }),
+      ((error: Response) => {
+        this.loadingService.hide();
+        this.alertService.show(error.toString());
+      }));
+    }
+
 
 }
 
