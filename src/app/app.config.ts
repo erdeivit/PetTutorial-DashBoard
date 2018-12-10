031fd6a92c034205c750c040f28b73ffeb6f475f
Sergi <nogues.s.6@gmail.com>
2018-12-10 18:57:15 +0100

    Finished

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -36,6 +36,7 @@ export class AppConfig {
   public static get STUDENTS_URL(): string { return '/students'; }
   public static get COUNT_URL(): string { return '/count'; }
   public static get GROUPS_URL(): string { return '/groups'; }
+  public static get ASSISTANCE_URL(): string { return '/assistance'; }
   public static get QUESTIONNAIRES_URL(): string { return '/questionnaires'; }
   public static get QUESTIONS_URL(): string { return '/questions'; }
   public static get ANSWERS_URL(): string { return '/answers'; }
